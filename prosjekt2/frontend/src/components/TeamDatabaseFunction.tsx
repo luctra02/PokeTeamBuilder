d17b0fe1b99e72ec66c1747c93c7ccde737b212b
Lucas Tran <lucastra@stud.ntnu.no>
2023-11-29 23:58:44 +0100

    docs(comments in code): #33
    skrive kommentarer til koden for komponenter

--- prosjekt2/frontend/src/components/TeamDatabaseFunction.tsx
+++ prosjekt2/frontend/src/components/TeamDatabaseFunction.tsx
@@ -3,36 +3,47 @@ import { CREATE_TEAM, DELETE_FROM_TEAM } from '../graphql/mutations';
 import { Pokemon, Team } from '../utils/constants';
 import { GET_TEAM } from '../graphql/queries';
 
+
 interface TeamDatabaseFunctionProps {
-  pokemon: Pokemon;
-  buttonText: string;
-  isButtonDisabled: boolean;
-  changeTeam: () => void;
+  pokemon: Pokemon; 
+  buttonText: string; 
+  isButtonDisabled: boolean; 
+  changeTeam: () => void; 
 }
 
-function TeamDatabaseFunction({pokemon, buttonText, isButtonDisabled, changeTeam}: TeamDatabaseFunctionProps)  {
+function TeamDatabaseFunction({
+  pokemon,
+  buttonText,
+  isButtonDisabled,
+  changeTeam,
+}: TeamDatabaseFunctionProps) {
+  // Retrieving or generating a unique team ID from local storage
   let teamId = localStorage.getItem('teamId');
   if (!teamId) {
-    teamId = Math.random().toString(36).slice(2, 11); 
+    teamId = Math.random().toString(36).slice(2, 11);
     localStorage.setItem('teamId', teamId);
   }
 
+  // Mutation hook for adding a Pokemon to the team
   const [createTeam] = useMutation(CREATE_TEAM, {
     update: (cache, { data: { createTeam } }) => {
       if (createTeam !== 'Team is full') {
+        // Reading the existing team data from the cache
         const existingData = cache.readQuery<{ getTeam: Team }>({
           query: GET_TEAM,
           variables: { teamId: teamId },
         });
-  
+
         if (existingData?.getTeam) {
           const currentTeam = existingData.getTeam;
-  
+
+          // Updating the team with the new Pokemon
           const updatedTeam: Team = {
             ...currentTeam,
             pokemon: [...currentTeam.pokemon, createTeam],
           };
-  
+
+          // Writing the updated team data back to the cache
           cache.writeQuery<{ getTeam: Team }>({
             query: GET_TEAM,
             variables: { teamId: teamId },
@@ -42,46 +53,48 @@ function TeamDatabaseFunction({pokemon, buttonText, isButtonDisabled, changeTeam
       }
     },
   });
-  
-  
 
+  // Mutation hook for deleting a Pokemon from the team
   const [pokemonDeleteFromTeam] = useMutation(DELETE_FROM_TEAM, {
     update: (cache, { data: { deletePokemonFromTeam } }) => {
+      // Reading the existing team data from the cache
       const existingData = cache.readQuery<{ getTeam: Team }>({
         query: GET_TEAM,
         variables: { teamId: teamId },
       });
-  
+
       if (existingData?.getTeam) {
         const currentTeam = existingData.getTeam;
-  
+
+        // Updating the team by removing the specified Pokemon
         const updatedPokemon = currentTeam.pokemon.filter(
           (poke) => poke.name !== deletePokemonFromTeam
         );
-        
+
         const updatedTeam: Team = {
           ...currentTeam,
           pokemon: updatedPokemon,
         };
-        
+
+        // Writing the updated team data back to the cache
         cache.writeQuery<{ getTeam: Team }>({
           query: GET_TEAM,
           variables: { teamId: teamId },
           data: { getTeam: updatedTeam },
         });
-        
       }
     },
   });
-  
-  
+
+  // Function to add the current Pokemon to the team
   function AddTeamToDatabase() {
-    createTeam({ 
-      variables: { 
-        teamInput: { 
-          teamId: teamId, pokemon: { 
+    createTeam({
+      variables: {
+        teamInput: {
+          teamId: teamId,
+          pokemon: {
             id: pokemon.id,
-            name: pokemon.name,                                         
+            name: pokemon.name,
             image: pokemon.image,
             types: pokemon.types,
             weight: pokemon.weight,
@@ -92,29 +105,36 @@ function TeamDatabaseFunction({pokemon, buttonText, isButtonDisabled, changeTeam
               hp: pokemon.baseStats.hp,
               specialattack: pokemon.baseStats.specialattack,
               specialdefense: pokemon.baseStats.specialdefense,
-              speed: pokemon.baseStats.speed
-            }, 
-          }
-        }
-      }                               
+              speed: pokemon.baseStats.speed,
+            },
+          },
+        },
+      },
     });
   }
 
+  // Function to delete the current Pokemon from the team
   function DeletePokemonFromTeam() {
-    pokemonDeleteFromTeam({ variables:  {
-      name: pokemon.name,
-      teamId: teamId
-     } });
+    pokemonDeleteFromTeam({ variables: { name: pokemon.name, teamId: teamId } });
   }
 
+  // Rendering the button with appropriate styles and event handling
   return (
-    <button style={buttonText === 'Add to Team' ? { backgroundColor: 'rgba(0, 255, 0, 0.8)' } : buttonText === 'Remove from Team' ? { backgroundColor: 'rgba(255, 0, 0, 0.8)' } : { backgroundColor: 'rgba(255, 255, 0, 0.5)' }}
+    <button
+      style={
+        buttonText === 'Add to Team'
+          ? { backgroundColor: 'rgba(0, 255, 0, 0.8)' }
+          : buttonText === 'Remove from Team'
+          ? { backgroundColor: 'rgba(255, 0, 0, 0.8)' }
+          : { backgroundColor: 'rgba(255, 255, 0, 0.5)' }
+      }
       className="teamButton"
       onClick={(e) => {
         e.stopPropagation();
-        if (buttonText == 'Add to Team') {
+        // Depending on the button text, either add or remove the Pokemon from the team
+        if (buttonText === 'Add to Team') {
           AddTeamToDatabase();
-        } else if (buttonText == 'Remove from Team') {
+        } else if (buttonText === 'Remove from Team') {
           DeletePokemonFromTeam();
         }
         changeTeam();
--- prosjekt2/frontend/src/components/TypesDropdown.tsx
+++ prosjekt2/frontend/src/components/TypesDropdown.tsx
@@ -4,52 +4,65 @@ import { useQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 import { GET_TYPES } from "../graphql/queries";
 
-
-function TypesDropdown(){  
-    const navigate = useNavigate()
+// Functional component representing a dropdown for filtering by types
+function TypesDropdown() {
+    const navigate = useNavigate();
     const [filterType, setFilterType] = useState("");
-    const [searchValue, setSearchValue] = useState("")
+    const [searchValue, setSearchValue] = useState("");
 
-    const searchStorage = sessionStorage.getItem('searchValue')
-    const typeStorage = sessionStorage.getItem('type')
+    // Retrieve filter type and search value from session storage on component mount
+    const searchStorage = sessionStorage.getItem('searchValue');
+    const typeStorage = sessionStorage.getItem('type');
     
     useEffect(() => {
+        // Set state variables based on session storage or default to empty string
         setFilterType(typeStorage ? JSON.parse(typeStorage) : '');
         setSearchValue(searchStorage ? JSON.parse(searchStorage) : '');
     }, [typeStorage, searchStorage]);
 
-    const {loading, data} = useQuery(GET_TYPES, {
-      variables: { search: searchValue}
-    })
+    // Fetch types data using Apollo Client's useQuery hook
+    const { loading, data } = useQuery(GET_TYPES, {
+        variables: { search: searchValue }
+    });
 
+    // Display a loading message while data is being fetched
     if (loading) {
-      return <div>Loading...</div>;
+        return <div>Loading...</div>;
     }
 
-    function handleFilter(type: string){
-        navigate('/')
-        sessionStorage.setItem('type', JSON.stringify(type))
+    // Function to handle filter selection and update session storage
+    function handleFilter(type: string) {
+        // Navigate to the root path when a filter is applied
+        navigate('/');
+        // Store the selected filter type in session storage
+        sessionStorage.setItem('type', JSON.stringify(type));
     }
 
-    return(
+    return (
         <FormControl fullWidth>
-          <InputLabel id="type" style={{ color: 'var(--text-color)' }}>Type</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={filterType}
-            className="dropdown"
-            label="Type"
-            onChange={(e) => handleFilter(e.target.value)}
-            style={{ color: 'var(--text-color)' }}
-          >
-              <MenuItem value="">All</MenuItem>
-              {data?.getTypes.types.map((type: string) => (
-                  <MenuItem value={type} key={type}>{type}</MenuItem>
-              ))}
-          </Select>
+            {/* Input label for the dropdown */}
+            <InputLabel id="type" style={{ color: 'var(--text-color)' }}>Type</InputLabel>
+            {/* Select component for the dropdown */}
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                // Controlled component: value is determined by filterType state
+                value={filterType}
+                className="dropdown"
+                label="Type"
+                // Event handler to update filter type and session storage
+                onChange={(e) => handleFilter(e.target.value)}
+                style={{ color: 'var(--text-color)' }}
+            >
+                {/* Default option for displaying all types */}
+                <MenuItem value="">All</MenuItem>
+                {/* Mapping over types data to generate dropdown options */}
+                {data?.getTypes.types.map((type: string) => (
+                    <MenuItem value={type} key={type}>{type}</MenuItem>
+                ))}
+            </Select>
         </FormControl>
-    )
+    );
 }
 
-export default TypesDropdown
+export default TypesDropdown;
