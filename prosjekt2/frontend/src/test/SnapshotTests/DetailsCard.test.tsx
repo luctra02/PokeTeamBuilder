2b58edcbcff2de6bc2de735557d34996c2e41bc1
Mats Nyfløt <mnyflot@gmail.com>
2023-11-29 16:26:35 +0100

    test(snapshots): #27
    Oppdater mocks slik at snapshot testene passerer.

--- prosjekt2/frontend/src/test/SnapshotTests/DetailsCard.test.tsx
+++ prosjekt2/frontend/src/test/SnapshotTests/DetailsCard.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import Details from '../../components/DetailsCardComponent';
 import { allMocks as mocks} from '../mocks/mockQueries';
 import { getPokemonsMock } from '../mocks/mockQueries';
@@ -12,7 +12,6 @@ describe('renders a pokemon', () => {
     it('matches the snapshot', async () => {
       const { asFragment } = testPageRender(<Details pokemon={getPokemonsMock.result.data.getPokemons.pokemons[0]} />, { mocks });  
       await waitFor(() => {
-        const imageUrl = 'https://play.pokemonshowdown.com/sprites/ani/pikachu.gif';
         const pikachuImage = screen.getByRole('img', { name: "" });
 
         expect(screen.getByText(/pikachu/)).toBeInTheDocument();
@@ -29,14 +28,4 @@ describe('renders a pokemon', () => {
       });
       expect(asFragment()).toMatchSnapshot();
     });
-  
-    /* it('test add to team button', async () => {
-      testPageRender(<Details pokemon={getPokemonsMock.result.data.getPokemons.pokemons[0]} />, { mocks });
-      const addToTeamButton = await screen.findByText(/Add to Team/);
-      userEvent.click(addToTeamButton);
-  
-      await waitFor(() => {
-        expect(screen.getByText(/Remove from Team/)).toBeInTheDocument();
-      });
-    }); */
   });
--- prosjekt2/frontend/src/test/SnapshotTests/DisplayCardComponents.test.tsx
+++ prosjekt2/frontend/src/test/SnapshotTests/DisplayCardComponents.test.tsx
@@ -1,63 +1,18 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import { allMocks as mocks} from '../mocks/mockQueries';
 import { testPageRender } from '../testUtils';
 import DisplayCardComponents from '../../components/DisplayCardComponents';
-import { GET_POKEMONS } from '../../graphql/queries';
-import { MockedProvider } from '@apollo/client/testing';
-import { MemoryRouter } from 'react-router-dom';
 
-const getPokemonsMock = [{
-    request: {
-      query: GET_POKEMONS,
-      variables: {
-        limit: 16,
-        offset: 0,
-        search: "",
-        sort: "id",
-        type: ""
-      },
-    },
-    result: {
-      data: {
-        getPokemons: {
-          count: 1,
-          pokemons: [
-            {
-                id: 25,
-                name: 'pikachu',
-                image: 'https://play.pokemonshowdown.com/sprites/ani/pikachu.gif',
-                types: ['Electric'],
-                weight: 6,
-                height: 0.4,
-                baseStats: {
-                    attack: 55,
-                    defense: 40,
-                    hp: 35,
-                    speed: 90,
-                    specialattack: 50,
-                    specialdefense: 50
-                }
-            },
-          ],
-        },
-      },
-    },
-  }
-];
+
+
 
 
 describe('renders a pokemon', () => {
     test('matches the snapshot', async () => {
         console.log("HEEER:", mocks)
-        const { asFragment } = render(
-            
-            <MockedProvider mocks={getPokemonsMock} addTypename={false}>
-                <MemoryRouter>
-                    <DisplayCardComponents />
-                </MemoryRouter>
-            </MockedProvider>
+        const { asFragment } = testPageRender(<DisplayCardComponents />, {mocks})
         
-        )
+        
   
         await waitFor(() => {
             expect(screen.getByText(/Pikachu/i)).toBeInTheDocument();
